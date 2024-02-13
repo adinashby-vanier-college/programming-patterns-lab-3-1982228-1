09b1d07fb842dab807594159ad7e4114f7b60829
Bridjette <1982228@edu.vaniercollege.qc.ca>
2024-02-13 10:31:06 -0500

    Modified and finished Lab 3.

--- src/main/java/com/prog2/labs/LabThree.java
+++ src/main/java/com/prog2/labs/LabThree.java
@@ -1,29 +1,71 @@
 package com.prog2.labs;
+
 /**
  * @author adinashby
  *
  */
-
 public class LabThree {
 
-	/**
-	 * Write your test code below in the main (optional).
-	 *
-	 */
-	public static void main(String[] args) {
+    /**
+     * Write your test code below in the main (optional).
+     *
+     */
+    public static void main(String[] args) {
 
-	}
-
-	/**
-	 * Please refer to the README file for question(s) description
-	 */
-
-	public static ListNode addTwoNumbers(ListNode firstList, ListNode secondList) {
-		return new ListNode();
-	}
-	
-    public static ListNode rotateRight(ListNode head, int k) {
-    	return new ListNode();
     }
 
+    /**
+     * Please refer to the README file for question(s) description
+     */
+    public static ListNode addTwoNumbers(ListNode firstList, ListNode secondList) {
+        ListNode copyHead = new ListNode(0);
+        ListNode currentNode = copyHead;
+        int carry = 0; 
+
+        while (firstList != null || secondList != null || carry != 0) {
+            int sum = carry;
+            if (firstList != null) {
+                sum += firstList.val;
+                firstList = firstList.next;
+            }
+            if (secondList != null) {
+                sum += secondList.val;
+                secondList = secondList.next;
+            }
+
+            carry = sum / 10;
+            currentNode.next = new ListNode(sum % 10);
+            currentNode = currentNode.next;
+        }
+
+        return copyHead.next;
+    }
+
+    public static ListNode rotateRight(ListNode head, int k) {
+        if (head == null || k == 0) {
+            return head;
+        }
+
+        ListNode current = head;
+        int len = 1;
+
+        while (current.next != null) {
+            current = current.next;
+            len++;
+        }
+
+        current.next = head;
+
+        ListNode newTail = head;
+
+        for (int i = 0; i < len - k % len - 1; i++) {
+            newTail = newTail.next;
+
+        }
+
+        ListNode newHead = newTail.next;
+        newTail.next = null;
+
+        return newHead;
+    }
 }
